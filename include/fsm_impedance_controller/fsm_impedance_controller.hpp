fa1c957be87b6da3c80471536fe531562ed9699f
zakaria.bouzit <zakaria.bouzit@accenture.com>
2024-02-09 15:17:48 +0100

    add on config activate desactivate

--- include/fsm_impedance_controller/fsm_impedance_controller.hpp
+++ include/fsm_impedance_controller/fsm_impedance_controller.hpp
@@ -1,5 +1,17 @@
-#ifndef FSM_IMPEDANCE_CONTROLLER_HPP
-#define FSM_IMPEDANCE_CONTROLLER_HPP
+/*
+Implement ControllerInterface class as follows:
+
+- If there are any member variables, initialized those in the constructor.
+- In the init() method, first call ControllerInterface::init() to initialize the lifecycle of the controller. Following this,
+declare all parameters defining their default values.
+- Implement the state_interface_configuration() and command_interface_configuration() methods.
+- Design the update() function for the controller. (real-time)
+
+- Add the required lifecycle management methods (others are optional):
+        on_configure() - reads parameters and configures controller.
+        on_activate() - called when controller is activated (started) (real-time)
+        on_deactivate() - called when controller is deactivated (stopped) (real-time)
+*/
 
 #include <memory>
 #include <mutex>
@@ -10,6 +22,7 @@
 #include <eigen3/Eigen/Dense>
 #include <functional>
 #include <thread>
+#include <cmath>
 
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp/node.hpp"
@@ -33,6 +46,8 @@
 #include "franka_semantic_components/franka_robot_model.hpp"
 #include "franka_semantic_components/franka_robot_state.hpp"
 
+#include <franka/robot_state.h>
+
 #include "fsm_impedance_controller/visibility_control.h"
 
 #define IDENTITY Eigen::MatrixXd::Identity(6,6) 
@@ -46,18 +61,15 @@ class FSMImpedanceController : public controller_interface::ControllerInterface
     
     controller_interface::InterfaceConfiguration command_interface_configuration() const override;
     controller_interface::InterfaceConfiguration state_interface_configuration() const override;
+
     CallbackReturn on_init() override;
-    void starting(const rclcpp::Time&);
     controller_interface::return_type update(const rclcpp::Time & time, const rclcpp::Duration & period) override;
 
+    CallbackReturn on_configure(const rclcpp_lifecycle::State& previous_state) override;
+    CallbackReturn on_activate(const rclcpp_lifecycle::State& previous_state) override;
+    CallbackReturn on_deactivate(const rclcpp_lifecycle::State& previous_state) override;
+
     private:
-
-    std::unique_ptr<franka_semantic_components::FrankaRobotState> state_handle_; // std::unique_ptr<franka_hw::FrankaStateHandle> state_handle_;
-    std::unique_ptr<franka_semantic_components::FrankaRobotModel> model_handle_; // std::unique_ptr<franka_hw::FrankaModelHandle> model_handle_;
-    std::vector<hardware_interface::CommandInterface> joint_handles_;// std::vector<hardware_interface::JointHandle> joint_handles_;
-
 };
 
 }
-
-#endif // FSM_IMPEDANCE_CONTROLLER_HPP
--- src/fsm_impedance_controller.cpp
+++ src/fsm_impedance_controller.cpp
@@ -1,20 +1,11 @@
 #include "fsm_impedance_controller/fsm_impedance_controller.hpp"
 #include <fsm_impedance_controller/robot_state.hpp>
 
-#include <cmath>
-#include <memory>
-
 #include <controller_interface/controller_interface.hpp>
 
-#include "rclcpp/rclcpp.hpp"
-#include "rclcpp/node.hpp"
-#include <franka/robot_state.h>
-
-#define IDENTITY Eigen::MatrixXd::Identity(6,6)
 
 namespace fsm_ic
 {
-
   controller_interface::InterfaceConfiguration FSMImpedanceController::command_interface_configuration() const
   {
     controller_interface::InterfaceConfiguration command_interfaces_config;
@@ -26,20 +17,29 @@ namespace fsm_ic
     return state_interfaces_config;
   }   
 
+
   CallbackReturn FSMImpedanceController::on_init()
   {
     return CallbackReturn::SUCCESS;
   }
-
-  void FSMImpedanceController::starting(const rclcpp::Time& /*time*/) 
-  {
-
-  }
-
-
   controller_interface::return_type FSMImpedanceController::update(const rclcpp::Time & time, const rclcpp::Duration & period)
   {
     return controller_interface::return_type::OK;
+  }
+
+  CallbackReturn FSMImpedanceController::on_configure(const rclcpp_lifecycle::State& /*previous_state*/)
+  {
+    return CallbackReturn::SUCCESS;
+  }
+
+  CallbackReturn FSMImpedanceController::on_activate(const rclcpp_lifecycle::State& /*previous_state*/) 
+  {
+    return CallbackReturn::SUCCESS;
+  }
+
+  controller_interface::CallbackReturn FSMImpedanceController::on_deactivate(const rclcpp_lifecycle::State& /*previous_state*/)
+  {
+    return CallbackReturn::SUCCESS;
   }
 
 }
