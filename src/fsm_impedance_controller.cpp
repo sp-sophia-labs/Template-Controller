8958b1477e86cf4be9695ba86ef9169abd7dbad1
zakaria.bouzit <zakaria.bouzit@accenture.com>
2024-02-13 15:11:13 +0100

    saturate_torque()

--- src/fsm_impedance_controller.cpp
+++ src/fsm_impedance_controller.cpp
@@ -27,6 +27,20 @@ namespace fsm_ic
     M_pinv_ = Eigen::MatrixXd(svd.matrixV() * S_.transpose() * svd.matrixU().transpose());
   }
 
+  Eigen::Matrix<double, 7, 1> FSMImpedanceController::saturateTorqueRate(
+    const Eigen::Matrix<double, 7, 1>& tau_d_calculated,
+    const Eigen::Matrix<double, 7, 1>& tau_J_d) 
+  {  
+    Eigen::Matrix<double, 7, 1> tau_d_saturated{};
+    for (size_t i = 0; i < 7; i++) {
+      double difference = tau_d_calculated[i] - tau_J_d[i];
+      tau_d_saturated[i] =
+        tau_J_d[i] + std::max(std::min(difference, delta_tau_max_), -delta_tau_max_);
+    }
+    return tau_d_saturated;
+  }
+
+
   controller_interface::InterfaceConfiguration FSMImpedanceController::command_interface_configuration() const
   {
     controller_interface::InterfaceConfiguration config;
