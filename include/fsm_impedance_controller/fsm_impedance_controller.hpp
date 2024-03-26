6f9cd7e93a4f3cc39dfbc7874b50329f707ff88e
Zakaria Bouzit <zakaria.bouzit@accenture.com>
2024-03-26 11:25:36 +0100

    update based on open source version

--- include/fsm_impedance_controller/fsm_impedance_controller.hpp
+++ include/fsm_impedance_controller/fsm_impedance_controller.hpp
@@ -1,18 +1,3 @@
-/*
-Implement ControllerInterface class as follows:
-
-- If there are any member variables, initialized those in the constructor.
-- In the init() method, first call ControllerInterface::init() to initialize the lifecycle of the controller. Following this,
-declare all parameters defining their default values.
-- Implement the state_interface_configuration() and command_interface_configuration() methods.
-- Design the update() function for the controller. (real-time)
-
-- Add the required lifecycle management methods (others are optional):
-        on_configure() - reads parameters and configures controller.
-        on_activate() - called when controller is activated (started) (real-time)
-        on_deactivate() - called when controller is deactivated (stopped) (real-time)
-*/
-
 #include <memory>
 #include <mutex>
 #include <string>
@@ -58,60 +43,90 @@ namespace fsm_ic
 class FSMImpedanceController : public controller_interface::ControllerInterface
 {
     public:
-    using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
-    
-    // Config methods
-    [[nodiscard]] controller_interface::InterfaceConfiguration command_interface_configuration() const override;
-    [[nodiscard]] controller_interface::InterfaceConfiguration state_interface_configuration() const override;
+        using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
 
-    // ROS2 lifecycle related methods
-    CallbackReturn on_init() override;
-    CallbackReturn on_configure(const rclcpp_lifecycle::State& previous_state) override;
-    CallbackReturn on_activate(const rclcpp_lifecycle::State& previous_state) override;
-    CallbackReturn on_deactivate(const rclcpp_lifecycle::State& previous_state) override;
+        // Config methods
+        [[nodiscard]] controller_interface::InterfaceConfiguration command_interface_configuration() const override;
+        [[nodiscard]] controller_interface::InterfaceConfiguration state_interface_configuration() const override;
 
-    // Main real-time method
-    controller_interface::return_type update(const rclcpp::Time & time, const rclcpp::Duration & period) override;
+        // ROS2 lifecycle related methods
+        CallbackReturn on_init() override;
+        CallbackReturn on_configure(const rclcpp_lifecycle::State& previous_state) override;
+        CallbackReturn on_activate(const rclcpp_lifecycle::State& previous_state) override;
+        CallbackReturn on_deactivate(const rclcpp_lifecycle::State& previous_state) override;
+
+        // Main real-time method
+        controller_interface::return_type update(const rclcpp::Time & time, const rclcpp::Duration & period) override;
+
+        // Custom methods
+        void update_stiffness_and_references();
 
 
     private:
-    std::unique_ptr<franka_semantic_components::FrankaRobotModel> franka_robot_model_;
-    std::unique_ptr<franka_semantic_components::FrankaRobotState> franka_robot_state_;
+        std::unique_ptr<franka_semantic_components::FrankaRobotModel> franka_robot_model_;
+        std::unique_ptr<franka_semantic_components::FrankaRobotState> franka_robot_state_;
 
-    bool k_elbow_activated{true};
+        bool k_elbow_activated{true};
+                
+        franka_msgs::msg::FrankaRobotState robot_state_, init_robot_state_;
+
+        const std::string k_robot_state_interface_name{"robot_state"};
+        const std::string k_robot_model_interface_name{"robot_model"};
+
+        std::string arm_id_{"fr3"};
+        int num_joints{7};
+
+        // Saturation
+        Eigen::Matrix<double, 7, 1> saturateTorqueRate(
+                const Eigen::Matrix<double, 7, 1>& tau_d_calculated,
+                const Eigen::Matrix<double, 7, 1>& tau_J_d); 
+
+
+        Eigen::Matrix<double, 6,6> Lambda = IDENTITY; // operational space mass matrix
+        Eigen::Matrix<double, 6, 6> Sm = IDENTITY; //task space selection matrix for positions and rotation
+        Eigen::Matrix<double, 6, 6> Sf = Eigen::MatrixXd::Zero(6,6); //task space selection matrix for forces
+        std::array<double, 16> F_T_EE; //end effector in flange frame
+        std::array<double, 16> EE_T_K; //stiffness frame in EE frame
+        Eigen::Affine3d pose_desired;
+        Eigen::Matrix<double, 6, 1> error; //pose error (6d)
+        Eigen::Matrix<double, 6, 1>  F_contact_des = Eigen::MatrixXd::Zero(6,6); //desired contact force
+        Eigen::Matrix<double, 6, 1>  F_ext = Eigen::MatrixXd::Zero(6,6); //external forces
+        Eigen::Matrix<double, 6, 1>  F_cmd = Eigen::MatrixXd::Zero(6,6); //commanded contact force
+        Eigen::Matrix<double, 6, 1>  I_F_error = Eigen::MatrixXd::Zero(6,6); //force error integral
+        Eigen::Matrix<double, 6,6> T = IDENTITY; // impedance inertia term
+        Eigen::Matrix<double, 6,6> K = IDENTITY; //impedance stiffness term
+        Eigen::Matrix<double, 6,6> D = IDENTITY; //impedance damping term
+        Eigen::Matrix<double, 6,6> cartesian_stiffness_target_; //impedance damping term
+        Eigen::Matrix<double, 6,6> cartesian_damping_target_; //impedance damping term
+        Eigen::Matrix<double, 6,6> cartesian_inertia_target_; //impedance damping term
+
+        //FLAGS
+        bool config_control = true; //sets if we want to control the configuration of the robot in nullspace
+        bool do_logging = false; //set if we do log values
+        // end FLAGS
+        double filter_params_{0.005};
+        double nullspace_stiffness_{40};
+        double nullspace_stiffness_target_{40.0};
+        const double delta_tau_max_{1.0};
+        Eigen::Matrix<double, 7, 1> q_d_nullspace_;
+        Eigen::Vector3d position_d_;
+        Eigen::Quaterniond orientation_d_;
+        std::mutex position_and_orientation_d_target_mutex_;
+        Eigen::Vector3d position_d_target_;
+        Eigen::Quaterniond orientation_d_target_;
         
-    franka_msgs::msg::FrankaRobotState robot_state_, init_robot_state_;
+        double count = 0; //logging
+        double dt = 0.001;
 
-    const std::string k_robot_state_interface_name{"robot_state"};
-    const std::string k_robot_model_interface_name{"robot_model"};
+        //repulsion sphere test;
+        double R;
+        Eigen::Vector3d C;
+        Eigen::Matrix<double, 3, 3> repulsion_K, repulsion_D;
+        Eigen::Matrix<double, 6, 1> F_repulsion;
+        Eigen::Matrix<double, 6, 1> F_impedance; 
 
-    std::string arm_id_{"fr3"};
-    int num_joints{7};
-
-    // Saturation
-    Eigen::Matrix<double, 7, 1> saturateTorqueRate(
-            const Eigen::Matrix<double, 7, 1>& tau_d_calculated,
-            const Eigen::Matrix<double, 7, 1>& tau_J_d); 
-
-    // Classic cartesian controller 
-    double filter_params_{0.005};
-    double nullspace_stiffness_{20.0};
-    double nullspace_stiffness_target_{20.0};
-    const double delta_tau_max_{1.0};
-    
-    Eigen::Matrix<double, 6, 6> cartesian_stiffness_;
-    Eigen::Matrix<double, 6, 6> cartesian_stiffness_target_;
-    Eigen::Matrix<double, 6, 6> cartesian_damping_;
-    Eigen::Matrix<double, 6, 6> cartesian_damping_target_;
-    Eigen::Matrix<double, 7, 1> q_d_nullspace_;
-    Eigen::Vector3d position_d_;
-    Eigen::Quaterniond orientation_d_;
-    std::mutex position_and_orientation_d_target_mutex_;
-    Eigen::Vector3d position_d_target_;
-    Eigen::Quaterniond orientation_d_target_;   
-
-    rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr sub_equilibrium_pose_;
-    void equilibriumPoseCallback(const geometry_msgs::msg::PoseStamped::SharedPtr msg);
+        rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr sub_equilibrium_pose_;
+        void equilibriumPoseCallback(const geometry_msgs::msg::PoseStamped::SharedPtr msg);
 };
 
 }
--- src/fsm_impedance_controller.cpp
+++ src/fsm_impedance_controller.cpp
@@ -18,22 +18,29 @@ namespace fsm_ic
       "equilibrium_pose", 20, 
       std::bind(&FSMImpedanceController::equilibriumPoseCallback, this, std::placeholders::_1));
 
+
+    std::vector<double> cartesian_stiffness_vector;
+    std::vector<double> cartesian_damping_vector;
     position_d_.setZero();
     orientation_d_.coeffs() << 0.0, 0.0, 0.0, 1.0;
     position_d_target_.setZero();
     orientation_d_target_.coeffs() << 0.0, 0.0, 0.0, 1.0;
-    
+
     // Compliance parameters
-    const double translational_stiffness{150.0};
-    const double rotational_stiffness{10.0};
-    cartesian_stiffness_.setZero();
-    cartesian_stiffness_.topLeftCorner(3, 3) << translational_stiffness * Eigen::MatrixXd::Identity(3, 3);
-    cartesian_stiffness_.bottomRightCorner(3, 3) << rotational_stiffness * Eigen::MatrixXd::Identity(3, 3);
-    cartesian_damping_.setZero();
-    cartesian_damping_.topLeftCorner(3, 3) << 2.0 * sqrt(translational_stiffness) *
-                                      Eigen::MatrixXd::Identity(3, 3);
-    cartesian_damping_.bottomRightCorner(3, 3) << 2.0 * sqrt(rotational_stiffness) *
-                                          Eigen::MatrixXd::Identity(3, 3);
+    nullspace_stiffness_target_ = 30;
+    K.topLeftCorner(3, 3) = 200 * Eigen::Matrix3d::Identity();
+    K.bottomRightCorner(3, 3) << 90, 0, 0, 0, 90, 0, 0, 0, 80;
+    D.topLeftCorner(3, 3) = 35 * Eigen::Matrix3d::Identity();
+    D.bottomRightCorner(3, 3) << 15, 0, 0, 0, 15, 0, 0, 0, 12;
+    cartesian_stiffness_target_ = K;
+    cartesian_damping_target_ = D;
+
+    // construct repulsing sphere around 0, 0, 0
+    R = 0.00001; C << 0.0, 0, 0.0;
+    repulsion_K.setZero(); repulsion_D.setZero();
+    // set zero stiffness and damping for rotational velocities and positions
+    repulsion_K = Eigen::Matrix3d::Identity(); repulsion_D = Eigen::Matrix3d::Identity();
+    
     return CallbackReturn::SUCCESS;
   }
 
@@ -148,6 +155,9 @@ namespace fsm_ic
     transform.translation() = position;
     transform.rotate(orientation.toRotationMatrix());
 
+    Lambda = (jacobian * M.inverse() * jacobian.transpose()).inverse();
+    T = Lambda; // let robot behave with it's own physical inertia
+
     // compute error to desired pose
     // position error
     Eigen::Matrix<double, 6, 1> error;
@@ -166,8 +176,24 @@ namespace fsm_ic
     error.tail(3) << -transform.rotation() * error.tail(3); 
 
     // compute control
+    F_impedance = -Lambda * T.inverse() * (D * (jacobian * dq) + K * error);
+
+    // Force PID
+    // F_ext = Eigen::Map<Eigen::Matrix<double, 6, 1>>(robot_state.O_F_ext_hat_K.data()) * 0.999 + 0.001 * F_ext;
+    // I_F_error += dt*(F_contact_des - F_ext);
+    // F_cmd = 0.2 * (F_contact_des - F_ext) + 0.1 * I_F_error + F_contact_des - 0 *Sf * F_impedance; //no need to multiply with Sf since it is done afterwards anyway
+
+    Eigen::Matrix<double, 6, 1> o_f_ext_hat_k_data;
+    o_f_ext_hat_k_data << robot_state_.o_f_ext_hat_k.wrench.force.x, robot_state_.o_f_ext_hat_k.wrench.force.y,
+          robot_state_.o_f_ext_hat_k.wrench.force.z, robot_state_.o_f_ext_hat_k.wrench.torque.x,
+          robot_state_.o_f_ext_hat_k.wrench.torque.y, robot_state_.o_f_ext_hat_k.wrench.torque.z;
+    
+    F_ext = o_f_ext_hat_k_data* 0.999 + 0.001 * F_ext;
+    I_F_error += dt*(F_contact_des - F_ext);
+    F_cmd = 0.2 * (F_contact_des - F_ext) + 0.1 * I_F_error + F_contact_des - 0 *Sf * F_impedance;
+
     // allocate variables
-    Eigen::VectorXd tau_task(7), tau_nullspace(7), tau_d(7);
+    Eigen::VectorXd tau_task(7), tau_nullspace(7), tau_d(7), tau_impedance(7);
 
     // pseudoinverse for nullspace handling
     // kinematic pseuoinverse
@@ -181,36 +207,53 @@ namespace fsm_ic
       std::cerr << "Error during pseudo-inverse computation: " << e.what() << std::endl;
     }
     
-    // Cartesian PD control with damping ratio = 1
-    tau_task << jacobian.transpose() * 
-                (-cartesian_stiffness_ * error - cartesian_damping_ * (jacobian * dq));
+
+    //construct external repulsion force
+    Eigen::Vector3d r = position - C; // compute vector between EE and sphere
+    //ROS_INFO_STREAM("r is " << r.transpose());
+    double penetration_depth = std::max(0.0, R-r.norm());
+    //ROS_INFO_STREAM("penetration depth is " << penetration_depth);
+    Eigen::Vector3d v = (jacobian*dq).head(3);
+    v = v.dot(r)/r.squaredNorm() * r; //projected velocity
+    //ROS_INFO_STREAM("projected velocity is " << v.transpose());
+    bool isInSphere = r.norm() < R;
+    //double alpha = error.norm()/(r.norm()+0.0001); //scaling to reach same forces in repulsion and add offset to not divide by 0
+    Eigen::Vector3d projected_error = error.head(3).dot(r)/r.squaredNorm() * r;
+    double r_eq = 0.8 * R;
+    //double alpha = projected_error.norm()*R*0.95;
+    repulsion_K = (K * r_eq/(R-r_eq)).topLeftCorner(3,3); //assume Lambda = Theta(T) to avoid numerical issues
+    repulsion_D = 2 * (repulsion_K).array().sqrt();
+
+    if(isInSphere){
+        F_repulsion.head(3) = 0.99* (repulsion_K * penetration_depth * r/r.norm() - repulsion_D * v) + 0.01 * F_repulsion.head(3); //assume Theta = Lambda
+    }
+    else{
+        double decay = -log(0.0001)/R; //at 2R the damping is divided by 10'000
+        F_repulsion.head(3) = - exp(decay * (R-r.norm())) * 0.1 * repulsion_D * v + 0.9 * F_repulsion.head(3); // 0.005 * F_repulsion_new + 0.995 * F_repulsion_old
+    }
+
     // nullspace PD control with damping ratio = 1
-    tau_nullspace << (Eigen::MatrixXd::Identity(7, 7) - 
-                      jacobian.transpose()* jacobian_transpose_pinv) * 
-                      (nullspace_stiffness_ * (q_d_nullspace_ - q) -
+    tau_nullspace << (Eigen::MatrixXd::Identity(7, 7) -
+                      jacobian.transpose() * jacobian_transpose_pinv) *
+                      (nullspace_stiffness_ * config_control * (q_d_nullspace_ - q) - //Do not use joint positions yet
                       (2.0 * sqrt(nullspace_stiffness_)) * dq);
 
-    // Desired torque                 
-    tau_d << tau_task + coriolis;
+    //virtual walls
+    double wall_pos = 12.0;
+    if (std::abs(position.y()) >= wall_pos){
+        F_impedance.y() = -(500 * (position.y()-wall_pos)) + 45 *(jacobian*dq)(1,0) * 0.001 + 0.999 * F_impedance(1,0);
+    }
+
+    tau_impedance = jacobian.transpose() * Sm * (F_impedance + F_repulsion) + jacobian.transpose() * Sf * F_cmd;
+    tau_d << tau_impedance + tau_nullspace + coriolis; //add nullspace and coriolis components to desired torque
+    tau_d << saturateTorqueRate(tau_d, tau_J_d);  // Saturate torque rate to avoid discontinuities
 
     for (int i = 0; i < num_joints; i++) {
       command_interfaces_[i].set_value(tau_d[i]); 
       std::cout<<tau_d[i]<<std::endl;
     }
     
-    // update parameters changed online either through dynamic reconfigure or through the interactive target by filtering
-    cartesian_stiffness_ = 
-    filter_params_ * cartesian_stiffness_target_ + (1.0 - filter_params_) * cartesian_stiffness_;
-    cartesian_damping_ = 
-    filter_params_ * cartesian_damping_target_ + (1.0 - filter_params_) * cartesian_damping_;
-    nullspace_stiffness_ = 
-    filter_params_ * nullspace_stiffness_target_ + (1.0 - filter_params_) * nullspace_stiffness_;
-    
-    std::lock_guard<std::mutex> position_d_target_mutex_lock(
-      position_and_orientation_d_target_mutex_);
-    position_d_ = filter_params_ * position_d_target_ + (1.0 - filter_params_) * position_d_;
-    orientation_d_ = orientation_d_.slerp(filter_params_, orientation_d_target_);
-    
+    update_stiffness_and_references();
     return controller_interface::return_type::OK;
   }
 
@@ -236,6 +279,21 @@ namespace fsm_ic
     }
     return tau_d_saturated;
   }
+
+  void FSMImpedanceController::update_stiffness_and_references(){
+    // update parameters changed online either through dynamic reconfigure or through the interactive
+    // target by filtering
+    /** at the moment we do not use dynamic reconfigure and control the robot via D, K and T **/
+    K = filter_params_ * cartesian_stiffness_target_ + (1.0 - filter_params_) * K;
+    D = filter_params_ * cartesian_damping_target_ + (1.0 - filter_params_) * D;
+    nullspace_stiffness_ =
+            filter_params_ * nullspace_stiffness_target_ + (1.0 - filter_params_) * nullspace_stiffness_;
+    std::lock_guard<std::mutex> position_d_target_mutex_lock(
+            position_and_orientation_d_target_mutex_);
+    position_d_ = filter_params_ * position_d_target_ + (1.0 - filter_params_) * position_d_;
+    orientation_d_ = orientation_d_.slerp(filter_params_, orientation_d_target_);
+  }
+  
 }
 
 // Expose the controller as visible to the rest of ros2_control
