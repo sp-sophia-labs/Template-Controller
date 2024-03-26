6b8eb0d1008d4aadc9e0dea1432c5bc491686838
Zakaria Bouzit <zakaria.bouzit@accenture.com>
2024-03-26 10:50:41 +0100

    adapt eth to open sourced version

--- src/fsm_impedance_controller.cpp
+++ src/fsm_impedance_controller.cpp
@@ -1,101 +1,24 @@
 #include "fsm_impedance_controller/fsm_impedance_controller.hpp"
-#include <fsm_impedance_controller/robot_state.hpp>
-
-#include <cmath>
-#include <memory>
-
-#include <controller_interface/controller_interface.hpp>
-
-#include "rclcpp/rclcpp.hpp"
-#include "rclcpp/node.hpp"
-#include <franka/robot_state.h>
-
-#define IDENTITY Eigen::MatrixXd::Identity(6,6)
+#include "fsm_impedance_controller/pseudo_inverse.hpp"
 
 namespace fsm_ic
 {
-  namespace {
-    template <class T, size_t N>
-    std::ostream& operator<<(std::ostream& ostream, const std::array<T, N>& array) {
-        //ostream << "[";
-        std::copy(array.cbegin(), array.cend() - 1, std::ostream_iterator<T>(ostream, ","));
-        std::copy(array.cend() - 1, array.cend(), std::ostream_iterator<T>(ostream));
-        //ostream << "]";
-        return ostream;
-    }
-  }  // anonymous namespace
-
-  inline void pseudoInverse(const Eigen::MatrixXd& M_, Eigen::MatrixXd& M_pinv_, bool damped = true) {
-      double lambda_ = damped ? 0.2 : 0.0;
-
-      Eigen::JacobiSVD<Eigen::MatrixXd> svd(M_, Eigen::ComputeFullU | Eigen::ComputeFullV);
-      Eigen::JacobiSVD<Eigen::MatrixXd>::SingularValuesType sing_vals_ = svd.singularValues();
-      Eigen::MatrixXd S_ = M_;  // copying the dimensions of M_, its content is not needed.
-      S_.setZero();
-
-      for (int i = 0; i < sing_vals_.size(); i++)
-          S_(i, i) = (sing_vals_(i)) / (sing_vals_(i) * sing_vals_(i) + lambda_ * lambda_);
-
-      M_pinv_ = Eigen::MatrixXd(svd.matrixV() * S_.transpose() * svd.matrixU().transpose());
-  }
-
-  controller_interface::InterfaceConfiguration FSMImpedanceController::command_interface_configuration() const
-  {
-    controller_interface::InterfaceConfiguration command_interfaces_config;
-    return command_interfaces_config;
-  }
-  controller_interface::InterfaceConfiguration FSMImpedanceController::state_interface_configuration() const
-  {
-    controller_interface::InterfaceConfiguration state_interfaces_config;
-    return state_interfaces_config;
-  }   
-
+  
   CallbackReturn FSMImpedanceController::on_init()
   {
+    // Equilibrium pose subscription
+    sub_equilibrium_pose_ = get_node()->create_subscription<geometry_msgs::msg::PoseStamped>(
+      "equilibrium_pose", 20, 
+      std::bind(&FSMImpedanceController::equilibriumPoseCallback, this, std::placeholders::_1));
+
     std::vector<double> cartesian_stiffness_vector;
     std::vector<double> cartesian_damping_vector;
     position_d_.setZero();
     orientation_d_.coeffs() << 0.0, 0.0, 0.0, 1.0;
     position_d_target_.setZero();
     orientation_d_target_.coeffs() << 0.0, 0.0, 0.0, 1.0;
-    return CallbackReturn::SUCCESS;
-  }
 
-  void FSMImpedanceController::starting(const rclcpp::Time& /*time*/) 
-  {
-    // compute initial velocity with jacobian and set x_attractor and q_d_nullspace
-    // to initial configuration
-    // franka::RobotState initial_state = state_handle_->getRobotState();
-    auto initial_state = std::make_shared<RobotStateListener>()->robot_state;
-
-    // get jacobian
-    std::array<double, 42> jacobian_array =
-            model_handle_->getZeroJacobian(franka::Frame::kEndEffector);
-
-    // convert to eigen
-    // Eigen::Map<Eigen::Matrix<double, 7, 1>> q_initial(initial_state.q.data());
-    Eigen::Map<Eigen::Matrix<double, 7, 1>> q_initial = Eigen::Map<Eigen::Matrix<double, 7, 1>>(initial_state->measured_joint_state.position.data());
-
-    // Eigen::Affine3d initial_transform(Eigen::Matrix4d::Map(initial_state.O_T_EE.data()));
-    geometry_msgs::msg::Pose initial_transform_Pose = initial_state->o_t_ee.pose;
-    // convert Pose to Eigen::Affine3d
-    Eigen::Translation3d translation(initial_transform_Pose.position.x, initial_transform_Pose.position.y, initial_transform_Pose.position.z);
-    Eigen::Quaterniond rotation(initial_transform_Pose.orientation.w, initial_transform_Pose.orientation.x, initial_transform_Pose.orientation.y, initial_transform_Pose.orientation.z);
-    Eigen::Affine3d initial_transform = translation * rotation;
-    
-    // Not sure which one we should use F_T_EE or f_t_ee.pose ?
-    // F_T_EE = initial_state.F_T_EE;
-    // EE_T_K = initial_state.EE_T_K;
-    geometry_msgs::msg::Pose F_T_EE = initial_state->f_t_ee.pose;
-    geometry_msgs::msg::Pose EE_T_K = initial_state->ee_t_k.pose;
-
-    position_d_ = initial_transform.translation();
-    orientation_d_ = Eigen::Quaterniond(initial_transform.rotation());
-    position_d_target_ = initial_transform.translation();
-    orientation_d_target_ = Eigen::Quaterniond(initial_transform.rotation());
-
-    // set nullspace equilibrium configuration to initial q
-    q_d_nullspace_ = q_initial;
+    // Compliance parameters
     nullspace_stiffness_target_ = 30;
     K.topLeftCorner(3, 3) = 200 * Eigen::Matrix3d::Identity();
     K.bottomRightCorner(3, 3) << 90, 0, 0, 0, 90, 0, 0, 0, 80;
@@ -103,48 +26,125 @@ namespace fsm_ic
     D.bottomRightCorner(3, 3) << 15, 0, 0, 0, 15, 0, 0, 0, 12;
     cartesian_stiffness_target_ = K;
     cartesian_damping_target_ = D;
-    //T.topLeftCorner(3, 3) = 1 * Eigen::Matrix3d::Identity();
-    //T.bottomRightCorner(3, 3) = 0.1 * Eigen::Matrix3d::Identity();
 
     // construct repulsing sphere around 0, 0, 0
     R = 0.00001; C << 0.0, 0, 0.0;
     repulsion_K.setZero(); repulsion_D.setZero();
     // set zero stiffness and damping for rotational velocities and positions
     repulsion_K = Eigen::Matrix3d::Identity(); repulsion_D = Eigen::Matrix3d::Identity();
+    
+    return CallbackReturn::SUCCESS;
   }
 
+  controller_interface::InterfaceConfiguration FSMImpedanceController::command_interface_configuration() const
+  {
+    // Define command interfaces
+    controller_interface::InterfaceConfiguration command_interfaces_config;
+    command_interfaces_config.type = controller_interface::interface_configuration_type::INDIVIDUAL;
+    
+    // get command config
+    for (int i = 1; i <= num_joints; i++) {
+      command_interfaces_config.names.push_back(arm_id_ + "_joint" + std::to_string(i) + "/effort");
+    }
+    return command_interfaces_config;
+  }
+
+  controller_interface::InterfaceConfiguration FSMImpedanceController::state_interface_configuration()const
+  {
+    // Define state interfaces
+    controller_interface::InterfaceConfiguration state_interfaces_config;
+    state_interfaces_config.type = controller_interface::interface_configuration_type::INDIVIDUAL;
+
+    // Creates state interface for robot state 
+    for (const auto& franka_robot_model_name : franka_robot_model_->get_state_interface_names()) {
+      state_interfaces_config.names.push_back(franka_robot_model_name);
+    }
+    // Creates state interface for robot model
+    for (const auto& franka_robot_state_name : franka_robot_state_->get_state_interface_names()) {
+      state_interfaces_config.names.push_back(franka_robot_state_name);
+    }
+    return state_interfaces_config;
+  }
+
+  CallbackReturn FSMImpedanceController::on_configure(const rclcpp_lifecycle::State& /*previous_state*/)
+  {
+    franka_robot_state_ = std::make_unique<franka_semantic_components::FrankaRobotState>(franka_semantic_components::FrankaRobotState(arm_id_ + "/" + k_robot_state_interface_name));
+    franka_robot_model_ = std::make_unique<franka_semantic_components::FrankaRobotModel>(franka_semantic_components::FrankaRobotModel(arm_id_ + "/" + k_robot_model_interface_name, arm_id_ + "/" + k_robot_state_interface_name));
+    return CallbackReturn::SUCCESS;
+  }
+
+  CallbackReturn FSMImpedanceController::on_activate(const rclcpp_lifecycle::State& /*previous_state*/) 
+  {
+    franka_robot_state_->assign_loaned_state_interfaces(state_interfaces_);
+    franka_robot_model_->assign_loaned_state_interfaces(state_interfaces_);
+
+    // Starting()
+    // compute initial velocity with jacobian and set x_attractor and q_d_nullspace
+    init_robot_state_ = franka_msgs::msg::FrankaRobotState();
+    franka_robot_state_->get_values_as_message(init_robot_state_);
+
+    Eigen::Map<Eigen::Matrix<double, 7, 1>> q_initial(init_robot_state_.measured_joint_state.position.data());
+
+    Eigen::Vector3d position_init(
+    init_robot_state_.o_t_ee.pose.position.x,
+    init_robot_state_.o_t_ee.pose.position.y,
+    init_robot_state_.o_t_ee.pose.position.z);
+    Eigen::Quaterniond orientation_init(
+    init_robot_state_.o_t_ee.pose.orientation.w,
+    init_robot_state_.o_t_ee.pose.orientation.x,
+    init_robot_state_.o_t_ee.pose.orientation.y,
+    init_robot_state_.o_t_ee.pose.orientation.z);
+    Eigen::Affine3d initial_transform = Eigen::Affine3d::Identity();
+    initial_transform.translation() = position_init;
+    initial_transform.rotate(orientation_init.toRotationMatrix());
+    position_d_ = initial_transform.translation();
+    orientation_d_ = initial_transform.rotation();
+    position_d_target_ = initial_transform.translation();
+    orientation_d_target_ = initial_transform.rotation();
+
+    q_d_nullspace_ = q_initial;
+    return CallbackReturn::SUCCESS;
+  }
+
+  controller_interface::CallbackReturn FSMImpedanceController::on_deactivate(const rclcpp_lifecycle::State& /*previous_state*/)
+  {
+    franka_robot_state_->release_interfaces();
+    franka_robot_model_->release_interfaces();
+    return CallbackReturn::SUCCESS;
+  }
 
   controller_interface::return_type FSMImpedanceController::update(const rclcpp::Time & time, const rclcpp::Duration & period)
   {
-    // get state variables
-    // franka::RobotState robot_state = state_handle_->getRobotState();
-    auto robot_state = std::make_shared<RobotStateListener>()->robot_state;
-    std::array<double, 49> mass = model_handle_->getMassMatrix();
-    std::array<double, 7> coriolis_array = model_handle_->getCoriolisForceVector();
-    std::array<double, 42> jacobian_array =
-              model_handle_->getZeroJacobian(franka::Frame::kEndEffector);
+    robot_state_ = franka_msgs::msg::FrankaRobotState();
+    franka_robot_state_->get_values_as_message(robot_state_);
 
-    // convert to Eigen
+    std::array<double, 49> mass = franka_robot_model_->getMassMatrix();
+    std::array<double, 7> coriolis_array = franka_robot_model_->getCoriolisForceVector();
+    std::array<double, 42> jacobian_array = franka_robot_model_->getZeroJacobian(franka::Frame::kEndEffector);
+
+    Eigen::Map<Eigen::Matrix<double, 7, 7>> M(mass.data());
     Eigen::Map<Eigen::Matrix<double, 7, 1>> coriolis(coriolis_array.data());
     Eigen::Map<Eigen::Matrix<double, 6, 7>> jacobian(jacobian_array.data());
-    Eigen::Map<Eigen::Matrix<double, 7, 1>> q(robot_state->measured_joint_state.position.data());
-    Eigen::Map<Eigen::Matrix<double, 7, 1>> dq(robot_state->measured_joint_state.velocity.data());
-    Eigen::Map<Eigen::Matrix<double, 7, 7>> M(mass.data());
-    Eigen::Map<Eigen::Matrix<double, 7, 1>> tau_J_d(  // NOLINT (readability-identifier-naming)
-            robot_state->desired_joint_state.effort.data());
-    
-    // Eigen::Affine3d transform(Eigen::Matrix4d::Map(robot_state.O_T_EE.data()));
-    geometry_msgs::msg::Pose transform_pose = robot_state->o_t_ee.pose;
-    // convert Pose to Eigen::Affine3d
-    Eigen::Translation3d translation(transform_pose.position.x, transform_pose.position.y, transform_pose.position.z);
-    Eigen::Quaterniond rotation(transform_pose.orientation.w, transform_pose.orientation.x, transform_pose.orientation.y, transform_pose.orientation.z);
-    Eigen::Affine3d transform = translation * rotation;
-    
-    Eigen::Vector3d position(transform.translation());
-    Eigen::Quaterniond orientation(transform.rotation());
+
+    Eigen::Map<Eigen::Matrix<double, 7, 1>> q(robot_state_.measured_joint_state.position.data());
+    Eigen::Map<Eigen::Matrix<double, 7, 1>> dq(robot_state_.measured_joint_state.velocity.data());
+    Eigen::Map<Eigen::Matrix<double, 7, 1>> tau_J_d(robot_state_.measured_joint_state.effort.data());
+    Eigen::Vector3d position(
+    robot_state_.o_t_ee.pose.position.x,
+    robot_state_.o_t_ee.pose.position.y,
+    robot_state_.o_t_ee.pose.position.z);
+    Eigen::Quaterniond orientation(
+    robot_state_.o_t_ee.pose.orientation.w,
+    robot_state_.o_t_ee.pose.orientation.x,
+    robot_state_.o_t_ee.pose.orientation.y,
+    robot_state_.o_t_ee.pose.orientation.z);
+    Eigen::Affine3d transform = Eigen::Affine3d::Identity();
+    transform.translation() = position;
+    transform.rotate(orientation.toRotationMatrix());
 
     Lambda = (jacobian * M.inverse() * jacobian.transpose()).inverse();
     T = Lambda; // let robot behave with it's own physical inertia
+    
     // compute error to desired pose
     // position error
     error.head(3) << position - position_d_;
@@ -163,19 +163,19 @@ namespace fsm_ic
     F_impedance = -Lambda * T.inverse() * (D * (jacobian * dq) + K * error);
 
     // Force PID
+    
+    // F_ext = Eigen::Map<Eigen::Matrix<double, 6, 1>>(robot_state.O_F_ext_hat_K.data()) * 0.999 + 0.001 * F_ext;
+    // I_F_error += dt*(F_contact_des - F_ext);
+    // F_cmd = 0.2 * (F_contact_des - F_ext) + 0.1 * I_F_error + F_contact_des - 0 *Sf * F_impedance; //no need to multiply with Sf since it is done afterwards anyway
+
     Eigen::Matrix<double, 6, 1> o_f_ext_hat_k_data;
-    o_f_ext_hat_k_data << robot_state->o_f_ext_hat_k.wrench.force.x, robot_state->o_f_ext_hat_k.wrench.force.y,
-          robot_state->o_f_ext_hat_k.wrench.force.z, robot_state->o_f_ext_hat_k.wrench.torque.x,
-          robot_state->o_f_ext_hat_k.wrench.torque.y, robot_state->o_f_ext_hat_k.wrench.torque.z;
+    o_f_ext_hat_k_data << robot_state_.o_f_ext_hat_k.wrench.force.x, robot_state_.o_f_ext_hat_k.wrench.force.y,
+          robot_state_.o_f_ext_hat_k.wrench.force.z, robot_state_.o_f_ext_hat_k.wrench.torque.x,
+          robot_state_.o_f_ext_hat_k.wrench.torque.y, robot_state_.o_f_ext_hat_k.wrench.torque.z;
     
     F_ext = o_f_ext_hat_k_data* 0.999 + 0.001 * F_ext;
     I_F_error += dt*(F_contact_des - F_ext);
     F_cmd = 0.2 * (F_contact_des - F_ext) + 0.1 * I_F_error + F_contact_des - 0 *Sf * F_impedance;
-
-    // F_ext = Eigen::Map<Eigen::Matrix<double, 6, 1>>(robot_state.O_F_ext_hat_K.data()) * 0.999 + 0.001 * F_ext;
-    // I_F_error += dt*(F_contact_des - F_ext);
-    // F_cmd = 0.2 * (F_contact_des - F_ext) + 0.1 * I_F_error + F_contact_des - 0 *Sf * F_impedance; //no need to multiply with Sf since it is done afterwards anyway
-    // //F_cmd = F_contact_des;
 
     // allocate variables
     Eigen::VectorXd tau_task(7), tau_nullspace(7), tau_d(7), tau_impedance(7);
@@ -183,7 +183,12 @@ namespace fsm_ic
     // pseudoinverse for nullspace handling
     // kinematic pseudoinverse
     Eigen::MatrixXd jacobian_transpose_pinv;
-    pseudoInverse(jacobian.transpose(), jacobian_transpose_pinv);
+    try {
+      // Release Build : colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release
+      pseudoInverse(jacobian.transpose(), jacobian_transpose_pinv);
+    } catch (const std::exception& e) {
+      std::cerr << "Error during pseudo-inverse computation: " << e.what() << std::endl;
+    }
 
     //construct external repulsion force
     Eigen::Vector3d r = position - C; // compute vector between EE and sphere
@@ -224,12 +229,42 @@ namespace fsm_ic
     tau_impedance = jacobian.transpose() * Sm * (F_impedance + F_repulsion) + jacobian.transpose() * Sf * F_cmd;
     tau_d << tau_impedance + tau_nullspace + coriolis; //add nullspace and coriolis components to desired torque
     tau_d << saturateTorqueRate(tau_d, tau_J_d);  // Saturate torque rate to avoid discontinuities
-    for (size_t i = 0; i < 7; ++i) {
-        joint_handles_[i].set_value(tau_d(i));
+
+    for (int i = 0; i < num_joints; i++) {
+      command_interfaces_[i].set_value(tau_d[i]); 
+      std::cout<<tau_d[i]<<std::endl;
     }
     
     update_stiffness_and_references();
     return controller_interface::return_type::OK;
+  }
+
+
+  void FSMImpedanceController::update_stiffness_and_references(){
+    // update parameters changed online either through dynamic reconfigure or through the interactive
+    // target by filtering
+    /** at the moment we do not use dynamic reconfigure and control the robot via D, K and T **/
+    K = filter_params_ * cartesian_stiffness_target_ + (1.0 - filter_params_) * K;
+    D = filter_params_ * cartesian_damping_target_ + (1.0 - filter_params_) * D;
+    nullspace_stiffness_ =
+            filter_params_ * nullspace_stiffness_target_ + (1.0 - filter_params_) * nullspace_stiffness_;
+    std::lock_guard<std::mutex> position_d_target_mutex_lock(
+            position_and_orientation_d_target_mutex_);
+    position_d_ = filter_params_ * position_d_target_ + (1.0 - filter_params_) * position_d_;
+    orientation_d_ = orientation_d_.slerp(filter_params_, orientation_d_target_);
+
+  }
+
+  Eigen::Matrix<double, 7, 1> FSMImpedanceController::saturateTorqueRate(
+          const Eigen::Matrix<double, 7, 1>& tau_d_calculated,
+          const Eigen::Matrix<double, 7, 1>& tau_J_d) {  // NOLINT (readability-identifier-naming)
+      Eigen::Matrix<double, 7, 1> tau_d_saturated{};
+      for (size_t i = 0; i < 7; i++) {
+          double difference = tau_d_calculated[i] - tau_J_d[i];
+          tau_d_saturated[i] =
+                  tau_J_d[i] + std::max(std::min(difference, delta_tau_max_), -delta_tau_max_);
+      }
+      return tau_d_saturated;
   }
 
 }
